eb723cf4e0595d21ac21f7d67074d818be3c15e4
Lawrence Johnson <jester124a@gmail.com>
2015-12-23 13:59:54 -0500

    Initial Commit

---
+++ tapper-extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-extreme
+//
+//  Created by Lawrence Johnson on 12/23/15.
+//  Copyright © 2015 Lawrence Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
