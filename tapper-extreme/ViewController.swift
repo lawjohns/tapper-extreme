18a54f4894fa8c01e98d41b16f3308ec3d9234b0
Lawrence Johnson <jester124a@gmail.com>
2015-12-25 14:29:03 -0500

    personal completion - has some errors

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var picker: UITextField!
+    @IBOutlet weak var coin: UIButton!
+    @IBOutlet weak var count: UILabel!
+    
+    var clickCount: Int = 0, currentCount: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startGame(sender: AnyObject) {
+        if !isnan(Double(picker.text!)!) {
+            clickCount = Int(picker.text!)!
+            logo.hidden = true
+            play.hidden = true
+            picker.hidden = true
+            coin.hidden = false
+            count.hidden = false
+            count.text = "0"
+            currentCount = 0
+        }
+    }
+    
+    @IBAction func addToCount(sender: AnyObject) {
+        if currentCount < (clickCount - 1) {
+            currentCount++
+            count.text = String(currentCount)
+        }
+        else {
+            endGame()
+        }
+    }
+    
+    func endGame() {
+        logo.hidden = false
+        play.hidden = false
+        picker.hidden = false
+        coin.hidden = true
+        count.hidden = true
+        picker.text = ""
+        currentCount = 0
+        clickCount = 0
+    }
 
 }
 
